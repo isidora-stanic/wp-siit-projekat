8c61a622cc4e03bde03ffc7c11e7f96f9068d711
matija <matija.matovic@uns.ac.rs>
2021-03-17 17:46:58 +0100

    Added main

---
+++ src/controller/Main.java
@@ -0,0 +1,13 @@
+package controller;
+
+import com.google.gson.Gson;
+import model.Kupac;
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
