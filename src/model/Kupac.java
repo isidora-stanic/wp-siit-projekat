273a67cc79a8b0b85d92bbabe9e57a59a0c51670
matija <matija.matovic@uns.ac.rs>
2021-03-17 17:33:36 +0100

    Definisane klase koje predstavaljaju korisnicke tipove

--- src/model/Kupac.java
+++ src/model/Kupac.java
@@ -1,4 +1,51 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class Kupac extends Korisnik {
+    private List<Karta> kupljeneKarte;
+    private int sakupljenihPoena;
+    private TipKorisnika.Tip tip;
+
+    public Kupac() {
+        super();
+        this.uloga = Uloga.KUPAC;
+        this.kupljeneKarte = new ArrayList<>();
+        this.sakupljenihPoena = 0;
+        this.tip = TipKorisnika.Tip.BRONZE;
+    }
+
+    public Kupac(String username, String password, String ime, String prezime, String pol, Date datumRodjenja) {
+        super(username, password, ime, prezime, pol, datumRodjenja);
+        this.uloga = Uloga.KUPAC;
+        this.kupljeneKarte = new ArrayList<>();
+        this.sakupljenihPoena = 0;
+        this.tip = TipKorisnika.Tip.BRONZE;
+    }
+
+    public List<Karta> getKupljeneKarte() {
+        return kupljeneKarte;
+    }
+
+    public void setKupljeneKarte(List<Karta> kupljeneKarte) {
+        this.kupljeneKarte = kupljeneKarte;
+    }
+
+    public int getSakupljenihPoena() {
+        return sakupljenihPoena;
+    }
+
+    public void setSakupljenihPoena(int sakupljenihPoena) {
+        this.sakupljenihPoena = sakupljenihPoena;
+    }
+
+    public TipKorisnika.Tip getTip() {
+        return tip;
+    }
+
+    public void setTip(TipKorisnika.Tip tip) {
+        this.tip = tip;
+    }
 }
--- src/model/Prodavac.java
+++ src/model/Prodavac.java
@@ -1,4 +1,29 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class Prodavac extends Korisnik {
+    private List<Manifestacija> manifestacije;
+
+    public Prodavac() {
+        super();
+        this.uloga = Uloga.PRODAVAC;
+        manifestacije = new ArrayList<>();
+    }
+
+    public Prodavac(String username, String password, String ime, String prezime, String pol, Date datumRodjenja) {
+        super(username, password, ime, prezime, pol, datumRodjenja);
+        this.uloga = Uloga.PRODAVAC;
+        manifestacije = new ArrayList<>();
+    }
+
+    public List<Manifestacija> getManifestacije() {
+        return manifestacije;
+    }
+
+    public void setManifestacije(List<Manifestacija> manifestacije) {
+        this.manifestacije = manifestacije;
+    }
 }
