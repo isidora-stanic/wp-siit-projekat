ca2f10b8c4b8a22512bda9b80e8a8311a2c06def
matija <matija.matovic99@gmail.com>
2021-05-04 20:27:47 +0200

    Dodat login i registracija

--- src/model/Administrator.java
+++ src/model/Administrator.java
@@ -1,4 +1,15 @@
 package model;
 
+import java.util.Date;
+
 public class Administrator extends Korisnik {
+    public Administrator() {
+        super();
+        this.uloga = Uloga.ADMIN;
+    }
+
+    public Administrator(String username, String password, String ime, String prezime, String pol, Date datumRodjenja) {
+        super(username, password, ime, prezime, pol, datumRodjenja);
+        this.uloga = Uloga.ADMIN;
+    }
 }
